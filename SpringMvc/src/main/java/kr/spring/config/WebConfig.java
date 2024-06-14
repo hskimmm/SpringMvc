76976a12439ce051fba7a3563eee7d7eba53ec01
hskim <fbhydfa@gmail.com>
2024-06-14 11:31:38 +0900

    Spring Security 적용 전 코드로 원복

--- SpringMvc/src/main/java/kr/spring/config/WebConfig.java
+++ SpringMvc/src/main/java/kr/spring/config/WebConfig.java
@@ -5,13 +5,14 @@ import javax.servlet.Filter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+
 //web.xml 설정 파일을 Java 파일로 변경
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
 	
 	//web.xml 에서 root-context.xml을 읽어들이는 부분(데이터베이스 설정 및 mapper 설정 부분)
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {RootConfig.class}; //RootConfig, SecurityConfig 클래스를 읽어들이고 메모리에 올림
+		return new Class[] {RootConfig.class, SecurityConfig.class}; //RootConfig, SecurityConfig 클래스를 읽어들이고 메모리에 올림
 	}
 
 	//web.xml 에서 dispatcherServelt이 servlet-context.xml을 읽어들이는 부분(resources 경로 설정 및 viewResolver 설정 및 컨트롤러 스캔 설정 부분) 
--- SpringMvc/src/main/java/kr/spring/controller/LoginController.java
+++ SpringMvc/src/main/java/kr/spring/controller/LoginController.java
@@ -3,6 +3,7 @@ package kr.spring.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
