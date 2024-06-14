86cf2c590f6b13d1d5b1b60d48fb51eefa945a0c
hskim <fbhydfa@gmail.com>
2024-06-14 11:20:27 +0900

    시큐리티 설정 삭제

--- SpringMvc/src/main/java/kr/spring/config/SecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package kr.spring.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-//스프링 시큐리티를 시작 시키는 클래스(스프링 시큐리티의 API를 메모리에 올리는 클래스)
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
-
-}
